ac99cce3f8f9c925f8fd3610fa567423362379ce
Pooja <83561650+jppooja26@users.noreply.github.com>
2023-06-30 17:21:52 +0530

    Add file via upload

---
+++ Matrix/spiral.java
@@ -0,0 +1,47 @@
+package Matrix;
+import java.util.Scanner;
+
+public class spiral {
+    public static void rotate(int[][] mat,int r,int c) {
+        int top=0,bottom=r-1;
+        int left=0,right=c-1;
+        while(top<=bottom && left<=right) {
+            for(int i=left;i<=right;i++) {
+                System.out.print(mat[top][i]+" ");
+            }
+            top++;
+            for(int i=top;i<=bottom;i++) {
+                System.out.print(mat[i][right]+" ");
+            }
+            right--;
+            if(top<=bottom) {
+                for(int i=right;i>=left;i--) {
+                    System.out.print(mat[bottom][i]+" ");
+                }
+                bottom--;
+            }
+            if(left<=right) {
+                for(int i=bottom;i>=top;i--) {
+                    System.out.print(mat[i][left]+" ");
+                }
+                left++;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter row: ");
+        int r = sc.nextInt();
+        System.out.print("Enter column: ");
+        int c = sc.nextInt();
+        int[][] mat = new int[r][c];
+        System.out.println("Enter matrix elements");
+        for(int i=0;i<r;i++) {
+            for(int j=0;j<c;j++) {
+                mat[i][j] = sc.nextInt();
+            }
+        }
+        System.out.print("Traversing spirally: ");
+        rotate(mat,r,c);
+    }
+}
