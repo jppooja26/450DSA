ecd645bfeb943f482065ded23e2ce5d83b94efea
Pooja <83561650+jppooja26@users.noreply.github.com>
2023-07-04 12:29:53 +0530

    Add file via upload

---
+++ String/palindrome.java
@@ -0,0 +1,24 @@
+//Time complexity - O(n)
+//Space complexity - O(n)
+package String;
+import java.util.Scanner;
+public class palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter string: ");
+        String str = sc.nextLine();
+        String nstr="";
+        char ch;
+        boolean found = false;
+        int n=str.length();
+        for(int i=0;i<n/2;i++) {
+            if(str.charAt(i)!=str.charAt(n-i-1)) {
+                found = true;
+                System.out.println(str+" is not a palindrome");
+                break;
+            }
+        }
+        if(found==false)
+            System.out.println(str+" is a palindrome");
+    }
+}
